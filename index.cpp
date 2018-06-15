5d8f2258ea4ed88f72dad7bed0983c96af1928c6
BoidoT <tomas.rhcp@gmail.com>
2018-06-15 14:25:24 -0300

    First Commit

---
+++ index.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+/*
+	Proyecto de Algoritmo y Estructura de Datos
+*/
+int main( int argc, const char* argv[] )
+{
+	printf( "\nHello World\n\n" );
+	return 0;
+}
