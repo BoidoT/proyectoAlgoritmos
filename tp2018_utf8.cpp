48ba6083b2f4486f4d70873d884ca4d08af63cce
Alejandro Lopez <alejandrolopez.exe@gmail.com>
2018-06-24 13:16:28 -0300

    Commit final a revisión 2
    -Correcciones para multiplataforma.

--- tp2018_utf8.cpp
+++ tp2018_utf8.cpp
@@ -11,66 +11,69 @@
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>  
-#include <Windows.h>
+#include <time.h>
+#ifdef _WIN32 //Solo incluir si se compila en windows
+  #include <Windows.h> //Libreria para setear la consola en utf8 en windows
+#endif
 #include <limits.h>
 
 using namespace std;
 
-/* DEFINICION DE STRUCTS */ 
+/* DEFINICION DE STRUCTS */
 struct seleccionNacional{
-  int numBolillero; 
+  int numBolillero;
   char nombreEquipo[30];
-  char confederacion[20]; 
+  char confederacion[20];
   bool asignadoGrupo; //Bandera para indicar que el equipo fue asignado a un grupo
 };
 
 struct grupoMundial{
   char identificador; //Letra del grupo
-  seleccionNacional equipos[4]; 
+  seleccionNacional equipos[4];
 };
 
 struct seleccionesAgrupadas{
   char nombreEquipo[30];
-  char confederacion[20]; 
+  char confederacion[20];
   char grupo;
 };
 
 /* PROTOTIPOS */
-bool escribirArchivo(const char[],seleccionNacional[],int); 
+bool escribirArchivo(const char[],seleccionNacional[],int);
 bool escribirArchivo(char[],seleccionNacional[],int);
 bool llenarEquipos(seleccionNacional[]);
 seleccionNacional llenarEquipo(int,const char[],const char[]);
 bool sortearEquipos(grupoMundial[],seleccionNacional[]);
 bool leerArchivo(char[],seleccionNacional[],int);
 bool leerGrupos(seleccionesAgrupadas[]);
-void ordenarEquipos(seleccionesAgrupadas[],int);      
-int menu(); 
+void ordenarEquipos(seleccionesAgrupadas[],int);
+int menu();
 
 int main(){
   int opcionMenu;
-  SetConsoleOutputCP(CP_UTF8); //Cambia el code page a UTF-8 para evitar problemas con carácteres no ingleses
-  
+  #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8); //Cambia el code page a UTF-8 para evitar problemas con carácteres no ingleses (solo windows)
+  #endif
   //Defino arrays de las estructuras que usaremos
   seleccionNacional selNacionales[32];
   grupoMundial gruposSorteo[8];
   seleccionesAgrupadas selAgrupadas[32];
 
   if(!llenarEquipos(selNacionales)){ //Defino los equipos
-    return 1; 
-  } 
+    return 1;
+  }
   if(!sortearEquipos(gruposSorteo,selNacionales)){ //Sorteo los grupos
-    return 1; 
-  } 
+    return 1;
+  }
   if(!leerGrupos(selAgrupadas)){ //Leo los archivos generados
-    return 1; 
-  } 
+    return 1;
+  }
   while(true){
     opcionMenu = menu();
     if(!opcionMenu){ // 0 (false) = salir
       return 0; //Termino la ejecucion correctamente
     }
-    ordenarEquipos(selAgrupadas,opcionMenu); //Pregunto al usuario como quiero ordenar y muestro el resultado 
+    ordenarEquipos(selAgrupadas,opcionMenu); //Pregunto al usuario como quiero ordenar y muestro el resultado
   }
 }
 
@@ -87,19 +90,23 @@ int menu(){
   do{
     cin >> opcion;
     if(!cin || opcion < 0 || opcion > 5){
-      cout << "Opcion no válida" << endl;      
+      cout << "Opcion no válida" << endl;
       cin.clear();
       cin.ignore(INT_MAX, '\n');
       opcion = -1;
       continue;
     }
-    system("cls"); //Limpio la consola     
+    #ifdef _WIN32
+      system("cls"); //Limpio la consola   (windows)
+    #else
+      system("clear"); //Limpio la consola  (unix)
+    #endif
     return opcion;
   }while(true);
 }
 
-/* Genera los 32 equipos 
-  Parametros: 
+/* Genera los 32 equipos
+  Parametros:
     selNacionales -> array de structs seleccionNacional a generar con los 32 equipos del mundial
   Retorna -> true si la operacin fue correcta, false si hubo un error al generar el archivo binario con los equipos
 */
@@ -112,7 +119,7 @@ bool llenarEquipos(seleccionNacional selNacionales[]){
   selNacionales[5] = llenarEquipo(1, "Bélgica", "UEFA");
   selNacionales[6] = llenarEquipo(1, "Polonia", "UEFA");
   selNacionales[7] = llenarEquipo(1, "Francia", "UEFA");
-  
+
   selNacionales[8] = llenarEquipo(2, "España", "UEFA");
   selNacionales[9] = llenarEquipo(2, "Perú", "Conmebol");
   selNacionales[10] = llenarEquipo(2, "Suiza", "UEFA");
@@ -121,7 +128,7 @@ bool llenarEquipos(seleccionNacional selNacionales[]){
   selNacionales[13] = llenarEquipo(2, "México", "Concacaf");
   selNacionales[14] = llenarEquipo(2, "Uruguay", "Conmebol");
   selNacionales[15] = llenarEquipo(2, "Croacia", "UEFA");
-  
+
   selNacionales[16] = llenarEquipo(3, "Dinamarca", "UEFA");
   selNacionales[17] = llenarEquipo(3, "Islandia", "UEFA");
   selNacionales[18] = llenarEquipo(3, "Costa Rica", "Concacaf");
@@ -147,11 +154,11 @@ bool llenarEquipos(seleccionNacional selNacionales[]){
   return true;
 }
 
-/* Crea un registro del tipo seleccionNacional y lo retorna 
- * Parametros: 
+/* Crea un registro del tipo seleccionNacional y lo retorna
+ * Parametros:
  *  bolillero -> numero de bolillero
  *  nombre -> nombre del equipo
- *  confederacion -> confederacion del equipo 
+ *  confederacion -> confederacion del equipo
  * Retorna -> un struct seleccionNacional
 */
 seleccionNacional llenarEquipo(int bolillero,const char nombre[],const char confederacion[]){
@@ -164,7 +171,7 @@ seleccionNacional llenarEquipo(int bolillero,const char nombre[],const char conf
 }
 
 /* Asigna los 32 equipos a los 8 grupos usando las reglas dadas
-  Parametros: 
+  Parametros:
     grupos -> array de struct grupoMundial donde se asignara 4 equipos a cada uno
     selecciones -> array de struct seleccionNacional con los 32 equipos a sortear
   Retorna -> true si la operacin fue correcta, false si hubo un error al generar los archivos binarios de los grupos
@@ -172,7 +179,7 @@ seleccionNacional llenarEquipo(int bolillero,const char nombre[],const char conf
 bool sortearEquipos(grupoMundial grupos[],seleccionNacional selecciones[]){
   srand (time(NULL)); // Inicializo mi funcin random con una seed del timestamp actual
   char letras[] = "ABCDEFGH";
-  char nombreGrupo[9]; //Aca se va a almacenar el nombre del archivo a generar 
+  char nombreGrupo[9]; //Aca se va a almacenar el nombre del archivo a generar
   int x,y; //Auxiliares
   int mismaConfederacion; //Variable contadora para evaluar equipos de la misma confederacion en un grupo
   int numEquipo; //Equipo sorteado
@@ -180,7 +187,7 @@ bool sortearEquipos(grupoMundial grupos[],seleccionNacional selecciones[]){
   for(int x=0;x<8;x++){
     grupos[x].identificador = letras[x];
   }
-  
+
   grupos[0].equipos[0] = selecciones[0]; // Asigno a Rusia al grupo A
   selecciones[0].asignadoGrupo = true; //Lo marco como asignado
 
@@ -193,30 +200,30 @@ bool sortearEquipos(grupoMundial grupos[],seleccionNacional selecciones[]){
     grupos[x].equipos[0] = selecciones[numEquipo]; //Asigno el equipo sorteado al grupo
     selecciones[numEquipo].asignadoGrupo = true; //Lo marco como asignado a un grupo
   }
-  
+
   //Bolillero 2 -- PASO 2
   for(x=0;x<=7;x++){
-    do{ 
+    do{
       numEquipo = rand() % 8 + 8; //Saco un numero aleatorio
       if(!selecciones[numEquipo].asignadoGrupo){
         //Evaluo si el equipo sorteado es un equipo comnebol y el equipo cabeza de serie es argentina o brasil
-        if(strcmp(selecciones[numEquipo].confederacion,"Conmebol") == 0 && 
+        if(strcmp(selecciones[numEquipo].confederacion,"Conmebol") == 0 &&
           (strcmp(grupos[x].equipos[0].nombreEquipo,"Argentina") == 0 ||
           strcmp(grupos[x].equipos[0].nombreEquipo,"Brasil") == 0))
         {
-          //En caso de que se cumpla la condicion anterior, deshacemos el paso 2 y volvemos a sortear el bolillero 
+          //En caso de que se cumpla la condicion anterior, deshacemos el paso 2 y volvemos a sortear el bolillero
           for(int y=8;y<=15;y++){
             selecciones[y].asignadoGrupo = false; //Desmarco la asignacion
             x=0;
           }
           continue; //Vuelvo a sacar un random
-        }  
+        }
         break; //Rompro el loop y asigno el equipo
       }
     }while(true);
     grupos[x].equipos[1] = selecciones[numEquipo];; //Asigno el equipo sorteado al grupo
     selecciones[numEquipo].asignadoGrupo = true;
-  }  
+  }
 
   //Bolillero 3 -- PASO 3
   for(x=0;x<=7;x++){
@@ -231,8 +238,8 @@ bool sortearEquipos(grupoMundial grupos[],seleccionNacional selecciones[]){
         //Si hay un equipo de la misma confederacion al sorteado en la posicion 2 del grupo (1 en el array) y lo marco en un contador
         if(strcmp(grupos[x].equipos[1].confederacion,selecciones[numEquipo].confederacion) == 0){
           mismaConfederacion++;
-        }   
-        //Si hay mas de un equipo de la misma confederacion en el grupo (2 en caso UEFA), desasigno los equipos sorteados y repito el paso 3                   
+        }
+        //Si hay mas de un equipo de la misma confederacion en el grupo (2 en caso UEFA), desasigno los equipos sorteados y repito el paso 3
         if((strcmp(selecciones[numEquipo].confederacion,"UEFA") != 0 && mismaConfederacion > 0) ||
           (strcmp(selecciones[numEquipo].confederacion,"UEFA") == 0 && mismaConfederacion > 1)){
           for(y=16;y<=23;y++){
@@ -240,13 +247,13 @@ bool sortearEquipos(grupoMundial grupos[],seleccionNacional selecciones[]){
             x=0;
           }
           continue; //Vuelvo a sacar un random
-        }       
+        }
         break; //Rompro el loop y asigno el equipo
       }
     }while(true);
     grupos[x].equipos[2] = selecciones[numEquipo]; //Asigno el equipo sorteado al grupo
     selecciones[numEquipo].asignadoGrupo = true; //Lo marco como asignado a un grupo
-  } 
+  }
 
   //Bolillero 4 -- PASO 4
   for(x=0;x<=7;x++){
@@ -258,15 +265,15 @@ bool sortearEquipos(grupoMundial grupos[],seleccionNacional selecciones[]){
         if(strcmp(grupos[x].equipos[0].confederacion,selecciones[numEquipo].confederacion) == 0){
           mismaConfederacion++;
         }
-        //Si hay un equipo de la misma confederacion al sorteado en la posicion 2 del grupo (1 en el array) y lo marco en un contador        
+        //Si hay un equipo de la misma confederacion al sorteado en la posicion 2 del grupo (1 en el array) y lo marco en un contador
         if(strcmp(grupos[x].equipos[1].confederacion,selecciones[numEquipo].confederacion) == 0){
           mismaConfederacion++;
-        }        
-        //Si hay un equipo de la misma confederacion al sorteado en la posicion 3 del grupo (2 en el array) y lo marco en un contador        
+        }
+        //Si hay un equipo de la misma confederacion al sorteado en la posicion 3 del grupo (2 en el array) y lo marco en un contador
         if(strcmp(grupos[x].equipos[2].confederacion,selecciones[numEquipo].confederacion) == 0){
           mismaConfederacion++;
-        }        
-        //Si hay mas de un equipo de la misma confederacion en el grupo (2 en caso UEFA), desasigno los equipos sorteados y repito el paso 4                   
+        }
+        //Si hay mas de un equipo de la misma confederacion en el grupo (2 en caso UEFA), desasigno los equipos sorteados y repito el paso 4
         if((strcmp(selecciones[numEquipo].confederacion,"UEFA") != 0 && mismaConfederacion > 0) ||
           (strcmp(selecciones[numEquipo].confederacion,"UEFA") == 0 && mismaConfederacion > 1))
         {
@@ -275,7 +282,7 @@ bool sortearEquipos(grupoMundial grupos[],seleccionNacional selecciones[]){
             x=0;
           }
           continue; //Vuelvo a sacar un random
-        }   
+        }
         break; //Rompro el loop y asigno el equipo
       }
     }while(true);
@@ -289,14 +296,14 @@ bool sortearEquipos(grupoMundial grupos[],seleccionNacional selecciones[]){
     strncat(nombreGrupo,&letras[x],1);
     if(!escribirArchivo(nombreGrupo,grupos[x].equipos,4)){
       return false;
-    }  
-  }     
+    }
+  }
   cout << "Grupos sorteados, 8 archivos binarios generados grupo_A.bin, grupo_B.bin, ..." << endl;
   return true;
 }
 
-/* Lee los archivos binarios con los grupos (grupo_A.bin, grupo_B.bin...) y los mete en un array de seleccionesAgrupadas 
- * Parametros: 
+/* Lee los archivos binarios con los grupos (grupo_A.bin, grupo_B.bin...) y los mete en un array de seleccionesAgrupadas
+ * Parametros:
  *  selAgrupadas -> Array de structs seleccionesAgrupadas a llenar
  * Retorna -> true si se pudo leer todos los archivos, false en caso contrario
 */
@@ -317,7 +324,7 @@ bool leerGrupos(seleccionesAgrupadas selAgrupadas[]){
       strcpy(selAgrupadas[(x*4)+y].nombreEquipo,selNacional[y].nombreEquipo);
       strcpy(selAgrupadas[(x*4)+y].confederacion,selNacional[y].confederacion);
       selAgrupadas[(x*4)+y].grupo = letras[x];
-    }    
+    }
   }
   return true;
 }
@@ -330,8 +337,8 @@ bool leerGrupos(seleccionesAgrupadas selAgrupadas[]){
 void ordenarEquipos(seleccionesAgrupadas selAgrupadas[],int modo){
   char letras[] = "ABCDEFGH";
   int x,y,z;
-  bool cambio; //Indica que se hizo un cambio 
-  seleccionesAgrupadas tmp; //Temporal para hacer el intercambio 
+  bool cambio; //Indica que se hizo un cambio
+  seleccionesAgrupadas tmp; //Temporal para hacer el intercambio
   switch(modo){
     case 1: //letra grupo
       do{ //Se repite hasta que no haya mas cambios
@@ -339,7 +346,7 @@ void ordenarEquipos(seleccionesAgrupadas selAgrupadas[],int modo){
         for(x=0;x<31;x++){
           for(y=x;y<32;y++){
             if(selAgrupadas[y].grupo < selAgrupadas[x].grupo){ //Comparo registros
-              tmp = selAgrupadas[y]; 
+              tmp = selAgrupadas[y];
               selAgrupadas[y] = selAgrupadas[x];
               selAgrupadas[x] = tmp;
               cambio = true; //Si hubo un cambio se marca la bandera para hacer un ciclo mas
@@ -361,7 +368,7 @@ void ordenarEquipos(seleccionesAgrupadas selAgrupadas[],int modo){
             }
           }
         }
-      }while(cambio);    
+      }while(cambio);
     break;
     case 3: //confederacion
       do{
@@ -376,7 +383,7 @@ void ordenarEquipos(seleccionesAgrupadas selAgrupadas[],int modo){
             }
           }
         }
-      }while(cambio);       
+      }while(cambio);
     break;
     case 4: //grupo y confederacion
       do{
@@ -399,9 +406,9 @@ void ordenarEquipos(seleccionesAgrupadas selAgrupadas[],int modo){
             }
           }
         }
-      }while(cambio);      
-    break;  
-    case 5: //grupo, confederacion y nombre   
+      }while(cambio);
+    break;
+    case 5: //grupo, confederacion y nombre
       do{
         cambio = false;
         for(x=0;x<31;x++){
@@ -425,33 +432,36 @@ void ordenarEquipos(seleccionesAgrupadas selAgrupadas[],int modo){
                   selAgrupadas[y] = selAgrupadas[x];
                   selAgrupadas[x] = tmp;
                   cambio = true;
-                }                
+                }
               }
             }
           }
         }
-      }while(cambio); 
+      }while(cambio);
     break;
-  }   
+  }
   //Muestro en pantalla el resultado
   for(x=0;x<32;x++){
     cout << x+1 << " Grupo: " << selAgrupadas[x].grupo << " " << selAgrupadas[x].nombreEquipo << " - " << selAgrupadas[x].confederacion << endl;
-  }      
-  system("pause"); //Para pausar el programa
+  }
+  cout << "Precione una tecla para volver al menú" << endl;
+  cin.clear();
+  cin.ignore(INT_MAX, '\n');
+  cin.get();
 }
 
-/* Escribe un archivo binario con los datos proporcionados 
- * Parametros: 
+/* Escribe un archivo binario con los datos proporcionados
+ * Parametros:
  *  nombreArchivo -> Nombre del archivo que se escribira
  *  selNacionales -> Array de structs seleccionNacional a grabar
  *  cant -> Cantidad de registros del array proporcionado a grabar
  * Retorna -> true si la escritura fue exitosa, false en caso contrario
- */ 
+ */
 
 /* Sobrecarga de funcion para que acepte el parametro nombreArchivo como un string constante (definido con comillas dobles) sin tirar warning al compilar */
 
 bool escribirArchivo(const char nombreArchivo[],seleccionNacional selNacionales[],int cant){
-  char _nombreArchivo[15]; 
+  char _nombreArchivo[15];
   strcpy(_nombreArchivo,nombreArchivo);
   return escribirArchivo(_nombreArchivo,selNacionales,cant);
 }
@@ -465,7 +475,7 @@ bool escribirArchivo(char nombreArchivo[],seleccionNacional selNacionales[],int 
   if(archivo == NULL){ //Si el puntero al archivo es null es que no se pudo abrir
     cout << "Error al abrir el archivo " << nombreArchivoExt << " en modo escritura" << endl;
     return false;
-  } 
+  }
   fwrite(selNacionales, sizeof(struct seleccionNacional), cant, archivo); //Grabo los struct en el archivo
   if(ferror(archivo)){ //Compruebo que se hayan podido grabar los datos (ferror() == 0)
     cout << "Error al escribir en el archivo "<< nombreArchivoExt << endl;
@@ -476,17 +486,17 @@ bool escribirArchivo(char nombreArchivo[],seleccionNacional selNacionales[],int 
 }
 
 /* Lee un archivo binario
- * Parametros: 
+ * Parametros:
  *  nombreArchivo -> Nombre del archivo que se escribira
  *  lecturaEquipos -> Array de structs seleccionNacional donde se volcaran los datos
  *  cant -> Cantidad de registros a leer
  * Retorna -> true si la lectura fue exitosa, false en caso contrario
- */ 
+ */
 bool leerArchivo(char nombreArchivo[],seleccionNacional lecturaEquipos[],int cant){
   char nombreArchivoExt[15]; //Nombre de archivo mas extension
   FILE *archivo; //Puntero al archivo
   strcpy(nombreArchivoExt,nombreArchivo);
-  strcat(nombreArchivoExt,".bin");  
+  strcat(nombreArchivoExt,".bin");
   archivo = fopen(nombreArchivoExt,"rb"); //Lo abro en modo lectura binaria
   if(archivo == NULL){
     cout << "Error al abrir el archivo "<< nombreArchivoExt << " en modo lectura" << endl;
@@ -497,6 +507,6 @@ bool leerArchivo(char nombreArchivo[],seleccionNacional lecturaEquipos[],int can
     cout << "Error al leer el archivo "<< nombreArchivoExt << endl;
     return false;
   }
-  fclose(archivo);       
+  fclose(archivo);
   return true;
 }
