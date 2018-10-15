07e84016236e2e673c7f5b48d7a6714c9ecb90a7
Alejandro Lopez <alejandrolopez.exe@gmail.com>
2018-10-15 20:59:15 -0300

    15/10/2018

--- AED-R21-PARTE2/tp-mod.cpp
+++ AED-R21-PARTE2/tp-mod.cpp
@@ -9,6 +9,12 @@
 
 using namespace std;
 
+//Equipos
+struct equipos{
+  int id;
+  char nombre[50];
+};
+
 //Para guardar en la matriz
 struct nodoLista{
   char nombreJugador[50];
@@ -29,13 +35,13 @@ struct registroGol{
 };
 
 
-registroGol* generarGoles(registroGol* &golPila, int cantidadGoles);
-registroGol* insertar(registroGol*& lista, int idGol, int fecha, int codEquipo, char *nombreJugador, int idPartido);
-int recorrerLista(registroGol*& listaGol);
-bool grabarListaEnArchivo(registroGol*&, char*);
-bool leerArchivo (registroGol*& listaGol, char* nombreArchivo);
-void generarMatrizGoles(registroGol*& listaGol, matriz mPartidos);
-void recorrerMatrizGoles (matriz mPartidos);
+registroGol* insertar(registroGol *&lista, int idGol, int fecha, int codEquipo, char *nombreJugador, int idPartido);
+bool leerDatosTexto(equipos [],registroGol *&);
+int recorrerLista(registroGol *&listaGol);
+bool grabarBinario(registroGol *&);
+bool leerBinario(registroGol *&);
+void generarMatrizGoles(registroGol *&, matriz);
+void recorrerMatrizGoles (matriz,equipos[]);
 
 int main(){
   #ifdef _WIN32
@@ -45,261 +51,261 @@ int main(){
   //Variables
 	registroGol* listaGol; //Lista dinamica que carga el Archivo
   registroGol* listaGolArchivo;
+  equipos e[32]; //Array de equipos
 	listaGol = NULL;
   listaGolArchivo = NULL;
-   matriz mPartidos;
-  generarGoles(listaGol, CANTIDADGOLES);
-  grabarListaEnArchivo(listaGol, (char *)"lista.bin");
-  leerArchivo(listaGolArchivo, (char *)"lista.bin");
+  matriz mPartidos;
+  leerDatosTexto(e,listaGol);
+  grabarBinario(listaGol);
+  leerBinario(listaGolArchivo);
   generarMatrizGoles(listaGolArchivo , mPartidos);
-  recorrerMatrizGoles ( mPartidos );
+  recorrerMatrizGoles(mPartidos,e);
   return 0;
 }
 
-void jugadorAleatorio(char jugador[]){
-  char jugadoresStr[] = "Christian Stuani,Edinson Cavani,Maximiliano Gómez,Luis Suárez,Jonathan Urretaviscaya,Giorgian de Arrascaeta,Fakhreddine Ben Youssef,Bassem Srarfi,Naim Sliti,Saber Khalifa,Wahbi Khazri,Ahmed Khalil,Ghaylane Chaâleli,Seifeddine Khaoui,Breel Embolo,Haris Seferovic,Mario Gavranovic,Josip Drmic,Marcus Berg,John Guidetti,Isaac Kiese Thelin,Ola Toivonen,Aleksandar Mitrovic,Aleksandar Prijovic,Luka Jovic,Nemanja Radonjic,Keita Baldé,Mame Biram Diouf,Moussa Konate,Sadio Mané,Mbaye Niang,Diafra Sakho,Moussa Sow,Ismaila Sarr,Artem Dzyuba,Aleksey Miranchuk,Fedor Smolov,André Silva,Cristiano Ronaldo,Gelson Martins,Gonçalo Guedes,Ricardo Quaresma,Dawid Kownacki,Robert Lewandowski,Arkadiusz Milik,Lukasz Teodorczyk,Jefferson Farfán,Paolo Guerrero,Raúl Ruidíaz,André Carrillo,Edison Flores,Ismael Díaz,Blas Pérez,Luis Tejada,Gabriel Torres,Abdiel Arroyo,Ahmed Musa,Kelechi Iheanacho,Victor Moses,Odion Ighalo,Alex Iwobi,Nwankwo Simeon,Abdullahi Shehu,Tyronne Ebuehi,Elderson Echiejile,Brian Idowu,Chidozie Awaziem,William Ekong,Leon Balogun,Kenneth Omeruo,Marco Fabián,Raúl Jiménez,Oribe Peralta,Javier Hernández,Carlos Vela,Hirving Lozano,Khalid Boutaib,Aziz Bouhaddouz,Ayoub El Kaabi,Youssef En Nesyri,Shinji Okazaki,Yuya Osako,Yoshinori Muto,Alfred Finnbogason,Bjorn Bergmann Sigurdarson,Jon Dadi Bodvarsson,Alireza Jahanbakhsh,Ashkan Dejagah,Mehdi Taremi,Reza Ghoochannejhad,Saman Ghoddos,Sardar Azmoun,Karim Ansarifard,Jamie Vardy,Marcus Rashford,Raheem Sterling,Danny Welbeck,Harry Kane,Ousmane Dembélé,Olivier Giroud,Antoine Griezmann,Kylian Mbappé,Thomas Lemar,Nabil Fekir,Florian Thauvin,Iago Aspas,Rodrigo,Diego Costa,Lucas Vázquez,Marwan Mohsen,Mohamed Salah,Mahmoud Kahraba,Ramadan Sobhy,Mahmoud Shikabala,Amr Warda,Pione Sisto,Martin Braithwaite,Andreas Cornelius,Viktor Fischer,Yussuf Poulsen,Nicolai Jorgensen,Kasper Dolberg,Mario Mandzukic,Ivan Perisic,Nikola Kalinic,Andrej Kramarić,Marko Pjaca,Ante Rebic,Johan Venegas,Joel Campbell,Marco Ureña,Kim Shin-wook,Son Heung-min,Hwang Hee-chan,Carlos Bacca,Miguel Ángel Borja,Radamel Falcao García,Luis Fernando Muriel,José Heriberto Izquierdo,Neymar,Gabriel Jesus,Firmino,Douglas Costa,Taison,Michy Batshuayi,Romelu Lukaku,Eden Hazard,Dries Mertens,Adnan Januzaj,Tim Cahill,Tomi Juric,Mathew Leckie,Daniel Arzani,Andrew Nabbout,Robbie Kruse,Dimi Petratos,Jaime MacLaren,Leo Messi,Paulo Dybala,Sergio Agüero,Gonzalo Higuaín,Fahad Al-Muwallad,Mohammad Al-Sahlawi,Muhannad Assiri,Mario Gómez,Timo Werner,Marco Reus,";  
-  char jugadores[167][50];
-  char temp[50],chr;
-  temp[0] = '\0';
-  jugador[0] = '\0';
-  //Aca llenamos un array de 167 jugadores para sortearlos
-  for(int x=0,z=0;x<strlen(jugadoresStr);x++){
-    chr = jugadoresStr[x];
-    if(chr == ','){ //Delimitador
-      strcpy(jugadores[z],temp);
-      temp[0] = '\0';
-      z++;
-    }else{
-      strncat(temp,&chr,1);
-    }
+//Funcion auxiliar que lee los archivos separados por comas: equipos.csv y goles.csv para armar las listas
+bool leerDatosTexto(equipos e[],registroGol *&golPila){
+  FILE *f;
+  char linea[60]; //Aca se lee cada linea
+  char temp[60]; //Usado para convertir char[] -> int
+  char *coma[3],*eof; //Puntero para separar los campos de cada linea
+  int x;
+  //Campos goles
+  int idGol;
+  int fecha;
+  int idEquipo;
+  int idPartido;
+  char nombreJugador[50];
+
+  //Leo archivo de texto de los equipos
+  f = fopen("equipos.csv", "r");
+  if(f == NULL) {
+    cout << "ERROR: No se pudo leer el archivo [equipos.csv]" << endl;
+    return false;
   }
-  strcpy(jugador,jugadores[rand() % 167]);
+  //Leo el archivo de texto
+  for(x = 0;x < 32 && fgets (linea, 60, f) != NULL;x++){
+    if(linea[0] == '#'){ //Ignoro encabezado
+      x--;
+      continue;
+    }
+    if (ferror(f)){ //Error al leer los datos
+      return false;
+    }       
+    coma[0] = strchr(linea,','); //Puntero a la posicion donde esta la coma para cortar el id de equipo
+    eof = strchr(linea,'\r'); //Puntero a la posicion donde esta el salto de linea (CRLF)
+    if(eof == NULL){
+      eof = strchr(linea,'\n'); //Puntero a la posicion donde esta el salto de linea (LF)
+    }
+    //Extraigo ID de equipo
+    temp[0] = '\0';
+    strncat(temp,linea,coma[0] - &linea[0]);
+    e[x].id = atoi(temp);
+    //Extraigo Nombre de equipo
+    temp[0] = '\0';
+    strncat(temp,coma[0]+1,eof - coma[0]-1); 
+    strncpy(e[x].nombre,temp,50); 
+  }
+  fclose(f);
+
+  //Leo archivo de texto de los goles
+  f = fopen("goles.csv", "r");
+  if(f == NULL) {
+    cout << "ERROR: No se pudo leer el archivo [goles.csv]" << endl;
+    return false;
+  }
+  //Leo el archivo de texto
+  for(x = 0;fgets (linea, 60, f) != NULL;x++){
+    if(linea[0] == '#'){ //Ignoro encabezado
+      x--;
+      continue;
+    }
+    if (ferror(f)){ //Error al leer los datos
+      return false;
+    }    
+    coma[0] = strchr(linea,','); //Puntero a la posicion donde esta la primera coma para cortar la fecha del partido
+    coma[1] = strchr(coma[0]+1,','); //Puntero a la posicion donde esta la segunda coma para cortar el jugador
+    coma[2] = strchr(coma[1]+1,','); //Puntero a la posicion donde esta la segunda coma para cortar el idpartido
+    eof = strchr(linea,'\r'); //Puntero a la posicion donde esta el salto de linea (CRLF)
+    if(eof == NULL){
+      eof = strchr(linea,'\n'); //Puntero a la posicion donde esta el salto de linea (LF)
+    }
+    //Fecha gol
+    temp[0] = '\0';
+    strncat(temp,linea,coma[0] - &linea[0]);
+    fecha = atoi(temp);
+    //Jugador
+    nombreJugador[0] = '\0';
+    strncat(nombreJugador,coma[0]+1,coma[1] - coma[0]-1);
+    //Id equipo
+    temp[0] = '\0';    
+    strncat(temp,coma[1]+1,coma[2] - coma[1]-1); 
+    idEquipo = atoi(temp);     
+    //ID partido
+    temp[0] = '\0';    
+    strncat(temp,coma[2]+1,eof - coma[2]-1); 
+    idPartido = atoi(temp);   
+    insertar(golPila,x,fecha,idEquipo,&nombreJugador[0],idPartido);
+  }
+  fclose(f);
+  return true;
 }
 
-registroGol* generarGoles(registroGol* &listaGol, int cantidadGoles){ //TODO: Yo lo haria VOID, la puta madre Andrea
-	registroGol* q=NULL;
-
-
-  srand (time(NULL));
-  cout<<"Cantidad Goles: "<<cantidadGoles<<endl;
-  for(int x=0;x<cantidadGoles;x++){
-    int codigoEquipo = rand() % 32+1;
-    int idDePartido = rand() % 64+1;
-    int fechaPartido = (idDePartido < 62 ? idDePartido/2 + 20180701 : 20180731 );
-    char jugador[50];
-    jugadorAleatorio(jugador);
-	  registroGol* p=new registroGol();
-	  insertar(listaGol,x+1,fechaPartido,codigoEquipo,&jugador[0],idDePartido);
-    
-	}
-}
-//Implementacion:
-//pLista siempre va a ser el principio de la lista
-//pNodo se usa para iterar
+//Inserta un registro en la lista de goles ordenado por equipo -> fecha
 registroGol* insertar(registroGol *&listaGol, int idGol, int fecha, int codEquipo, char *nombreJugador, int idPartido){
-  registroGol *pNodo;
+  registroGol *aux;
   //Si la lista no esta vacia, declaro un nuevo Nodo
-  registroGol* aux = new registroGol();
-  aux -> idGol = idGol;
-  aux -> codEquipo = codEquipo;
-  strcpy(aux -> nombreJugador,nombreJugador); 
-  aux -> fecha = fecha;  
-  aux -> idPartido = idPartido;  
-  aux -> siguiente = NULL;
+  registroGol* nuevoNodo = new registroGol();
+  nuevoNodo -> idGol = idGol;
+  nuevoNodo -> codEquipo = codEquipo;
+  strcpy(nuevoNodo -> nombreJugador,nombreJugador); 
+  nuevoNodo -> fecha = fecha;  
+  nuevoNodo -> idPartido = idPartido;  
+  nuevoNodo -> siguiente = NULL;
 
   //Si la lista esta vacia
   if(listaGol == NULL){ 
     //¿Por que devuelvo esto?
-    listaGol = aux;
-    return aux;
+    listaGol = nuevoNodo;
+    return nuevoNodo;
   }
 
-//Caso particular: Lista de un solo elemento. 
+  //Lista con un solo elemento
   if(listaGol -> codEquipo > codEquipo || (listaGol -> codEquipo == codEquipo && listaGol -> fecha > fecha)){
-    aux -> siguiente = listaGol;
-    listaGol = aux;
-    return aux;
+    nuevoNodo -> siguiente = listaGol;
+    listaGol = nuevoNodo;
+    return nuevoNodo;
   }
 
-  //Declaro pNodo que lo voy a usar para recorrer la lista hasta cierta condicion
-  pNodo = listaGol;
-
-  //Caso General, recorro.
-  while(pNodo -> siguiente != NULL 
-  && (pNodo -> siguiente -> codEquipo < codEquipo 
-  || (pNodo -> siguiente -> codEquipo == codEquipo && pNodo -> siguiente -> fecha <= fecha))){
-    pNodo = pNodo -> siguiente;
+  //Aux lo voy a usar para recorrer la lista hasta cierta condicion o llegar al final de la lista
+  aux = listaGol;
+  //Caso General, recorro hasta encontrar donde insertar el nodo
+  while(aux -> siguiente != NULL 
+  && (aux -> siguiente -> codEquipo < codEquipo 
+  || (aux -> siguiente -> codEquipo == codEquipo && aux -> siguiente -> fecha <= fecha))){
+    aux = aux -> siguiente;
   }
 
-  //Encontre donde tengo que insertarlo
-  aux -> siguiente = pNodo -> siguiente;
-  pNodo -> siguiente  = aux;
-  return aux;
+  //Inserto el nodo
+  nuevoNodo -> siguiente = aux -> siguiente;
+  aux -> siguiente = nuevoNodo;
+  return nuevoNodo;
 }
 
-//Graba en el Archivo toda la lista
-bool grabarListaEnArchivo (registroGol *&listaGol, char* nombreArchivo){
+//Graba en el Archivo binario toda la lista
+bool grabarBinario(registroGol *&listaGol){
   registroGol *pNodo;
   pNodo = listaGol;
   FILE* f;
-  f = fopen(nombreArchivo, "wb");
-
-  //Caso extremo
-  if (f == NULL){
+  f = fopen("datos.bin", "wb");
+  if (f == NULL){ //Error al abrir el archivo en modo escritura
     return false;
   }
-
-  while (pNodo != NULL)
-  {
+  while (pNodo != NULL){ //Recorro la lista y voy escribiendo el archivo binario
     fwrite(pNodo,sizeof(registroGol),1,f);
-    if (ferror(f))
-    { 
+    if (ferror(f)){ //Error al escribir los datos
       return false;
     }
     pNodo = pNodo->siguiente;
   }
-
   fclose(f);
   return true;
 }
 
 //Lee el Archivo toda la lista, en memoria
-bool leerArchivo (registroGol *&listaGol, char* nombreArchivo){
-  registroGol pNodo;
-  //pNodo = listaGol;
-  FILE* f;
-  f = fopen(nombreArchivo, "rb");
-
-  //Caso extremo
-  if (f == NULL){
+bool leerBinario(registroGol *&listaGol){
+  registroGol aux;
+  FILE* f = fopen("datos.bin", "rb");
+  if (f == NULL){ //Error al leer el archivo
     return false;
   }
   int registros;
   do{
-    registros = fread(&pNodo,sizeof(registroGol),1,f);
-    if(registros == 0){
+    registros = fread(&aux,sizeof(registroGol),1,f);
+    if(registros == 0){ //Cuando llego al final del archivo salir
       break;
     }
-    insertar(listaGol, pNodo.idGol, pNodo.fecha, pNodo.codEquipo, pNodo.nombreJugador, pNodo.idPartido);
-    if(ferror(f)){ 
+    insertar(listaGol, aux.idGol, aux.fecha, aux.codEquipo, aux.nombreJugador, aux.idPartido);
+    if(ferror(f)){ //Error de lectura
       return false;
     }
-  }while(1);
-
+  }while(true);
   fclose(f);
   return true;
 }
 
-int recorrerLista(registroGol*& listaGol){
-  registroGol *pNodo = listaGol; 
-  int cant = 0;
-  while(pNodo != NULL){
-    cout << "IDGOL: " << pNodo -> idGol << " EQUIPO: " << pNodo -> codEquipo << " FECHA: " << pNodo -> fecha << " JUGADOR: " << pNodo -> nombreJugador << " IDPARTIDO: " << pNodo -> idPartido << endl;
-    pNodo = pNodo -> siguiente;
-    cant++;
-  }  
-  return cant;
-}
-
-//DEBUG
-int recorrer(nodoLista*& nl){
-  nodoLista *pNodo = nl; 
-  int cant = 0;
-  if(nl == NULL){
-    cout << "Me pasaste un NULL" << endl;
-  }
-  while(pNodo != NULL){
-    cout << "Fecha: " << pNodo -> fecha << " Nomjugador: " << pNodo -> nombreJugador << " Goles: " << pNodo -> goles << endl;
-    pNodo = pNodo -> siguiente;
-    cant++;
-  }  
-  return cant;
-}
-
 void generarMatrizGoles(registroGol*& listaGol, matriz mPartidos){
-  int x;
-  nodoLista* auxNL = NULL;
+  int x,y;
+  bool crearNodo; //Bandera para indicar si se debe crear un nuevo nodo de gol en el partido actual (si el jugador coincide)
+  nodoLista* nuevoNodo = NULL,*auxNL;
   registroGol* auxRG = NULL;
-  for(int x=0 ; x<32; x++){
-    for(int y=0 ; y<7 ; y++){
+  for(x=0 ; x<32; x++){
+    for(y=0 ; y<7 ; y++){
       mPartidos[x][y] = NULL ;
     }
   }
-
-  bool nuevoNodo;
+  x=0;
   while ( listaGol != NULL ){
-    cout << "IDGOL: " << listaGol -> idGol << " EQUIPO: " << listaGol -> codEquipo << " FECHA: " << listaGol -> fecha << " JUGADOR: " << listaGol -> nombreJugador << " IDPARTIDO: " << listaGol -> idPartido << endl;
-    //Si la primera columna esta vacia (NULL) insertar ahi
+    //Si la primera columna esta vacia (NULL) insertar un nuevo nodo
     if(mPartidos[listaGol -> codEquipo -1][x] == NULL){
-      auxNL = new nodoLista();
-      strcpy(auxNL->nombreJugador, listaGol-> nombreJugador);
-      auxNL->fecha = listaGol -> fecha; 
-      auxNL->goles=1;
-      auxNL->siguiente=NULL;
-      mPartidos[listaGol -> codEquipo -1][x] = auxNL; //Y lo inserto en la columna
+      nuevoNodo = new nodoLista();
+      strcpy(nuevoNodo -> nombreJugador, listaGol-> nombreJugador);
+      nuevoNodo -> fecha = listaGol -> fecha; 
+      nuevoNodo -> goles = 1;
+      nuevoNodo -> siguiente = NULL;
+      mPartidos[listaGol -> codEquipo -1][x] = nuevoNodo; //Y lo inserto en la columna
       listaGol = listaGol -> siguiente; //Avanzo al siguiente registro
       x=0; //Reseteo el contador de columnas
-      cout << ">>> Lista vacia inserto nodo " << x << endl; 
       continue; //Repetir el LOOP
     }
-
     //Si el partido existentente en la columna no coincide con el leido avanzar a la siguiente columna
     if(mPartidos[listaGol -> codEquipo -1][x] -> fecha != listaGol -> fecha){
       x++; //Aumento en 1 el contador de columnas para buscar la siguiente
-      cout << ">>> Avanzo a la columna " << x << endl; 
       continue; //Repetir el LOOP
     }
-
     //Si llego aca es que encontro un segundo registro del mismo partido, hay que determinar si el gol es del mismo jugador o hay que agregar un nodo
-    auxNL = mPartidos[listaGol -> codEquipo -1][x];
-    nuevoNodo = true; //Si esta bandera queda en true, se suma gol y no se añade nodo
-    while(auxNL != NULL){ //Recorro la lista
+    auxNL = mPartidos[listaGol -> codEquipo -1][x]; //Hago una copia del principio de la lista 
+    crearNodo = true; //Si esta bandera queda en true, se suma gol y no se añade nodo
+    while(auxNL -> siguiente != NULL){ //Recorro la lista
       if(strcmp(auxNL -> nombreJugador,listaGol -> nombreJugador) == 0){ //Si el jugador es el mismo sumar los goles
-        cout << "SUME GOL " << listaGol -> nombreJugador << endl;  
-        nuevoNodo = false;            
         auxNL -> goles++;
+        crearNodo = false; //Marco la bandera en false para evitar que se cree un nodo
         break;
       }
       auxNL = auxNL -> siguiente;
     }
-    if(nuevoNodo){ // encontro un segundo registro del mismo partido y el jugador es distinto, agregar nuevo nodo
+    if(crearNodo){ // encontro un segundo registro del mismo partido y el jugador es distinto, agregar nuevo nodo
       //Si no encontre el jugador en la lista de goles del partido insertar un nuevo nodo
-      auxNL = new nodoLista();
-      strcpy(auxNL->nombreJugador, listaGol-> nombreJugador);
-      auxNL->fecha = listaGol -> fecha; 
-      auxNL->goles=1;
-      auxNL->siguiente=NULL;
-      cout << "MISMO PARTIDO - FECHA: " << auxNL -> fecha << " JUG: " << auxNL -> nombreJugador << endl;
+      nuevoNodo = new nodoLista();
+      strcpy(nuevoNodo -> nombreJugador, listaGol-> nombreJugador);
+      nuevoNodo -> fecha = listaGol -> fecha; 
+      nuevoNodo -> goles=1;
+      nuevoNodo -> siguiente=NULL;
+      auxNL -> siguiente = nuevoNodo; //Insertar el nuevo nodo al final de la lista
     }
-    //Si llego aca es que encontro un segundo registro del mismo partido y el jugador es distinto, agregar nuevo nodo
     listaGol = listaGol -> siguiente; //Avanzo al siguiente registro
     x=0; //Reseteo el contador de columnas
   }
-  cout << "Termine " << endl;
 }
 
-
-
-
-void recorrerMatrizGoles (matriz mPartidos){
+void recorrerMatrizGoles (matriz mPartidos,equipos e[]){
   nodoLista* auxNL = NULL;
   int cantGoles,z;
   for(int x=0; x<32; x++){
-    cout << "Equipo " << x+1 << endl;
+    cout << "Equipo " << e[x].nombre << endl;
     cantGoles=0;
     for(int y=0; y<7; y++){
       auxNL = mPartidos[x][y];
       if(auxNL == NULL){
         continue;
       }
-      cout << "\tPartido " << y+1 << " " << mPartidos[x][y] -> fecha << endl;
-      z=1;
+      cout << "\tPartido " << y+1 << " (" << mPartidos[x][y] -> fecha << ")" << endl;
       while(auxNL != NULL){
         cantGoles += auxNL->goles;
-        cout << "\t\tGol " << z << " > " << auxNL -> fecha << auxNL -> nombreJugador << endl << endl;
+        cout << "\t\t" << auxNL -> nombreJugador << " - Goles: " << auxNL-> goles << endl;
         auxNL = auxNL -> siguiente;
       }
     }
-  //  cout << "Cantidad de goles del Equipo " << x+1 <<": "<< cantGoles << endl;
+    cout << "\tTotal goles : " << cantGoles << "\n\n" << endl;
   }
 }
